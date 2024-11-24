6b423ee9084513a43112e7f55262c63bb04821c7
joaolsena <joaolsena129@gmail.com>
2024-11-23 23:07:21 -0300

    a

--- arcade.c
+++ arcade.c
@@ -16,14 +16,17 @@ typedef struct {
     int score;
 } Player;
 
-typedef struct {
-    int x;
-    int y;
-} Movimento;
+typedef enum {
+    TELA_MENU_PRINCIPAL,
+    TELA_SELECAO_JOGOS,
+    SAIR
+} Estado;
 
+Estado estado_atual = TELA_MENU_PRINCIPAL;
 
 const int opcao_y[] = {10, 15, 20};
-int opcao_atual = 0; 
+const int opcao_selecao_jogos_y[] = {5, 10, 15, 20};
+int opcao_atual = 0;
 
 int kbhit(void) {
     struct termios oldt, newt;
@@ -62,9 +65,12 @@ int getch(void) {
     return move;
 }
 
+void limpar() {
+    system("clear");
+}
 
-void desenharTela() {
-    system("clear"); 
+void desenharMenuPrincipal() {
+    limpar();
     for (int i = 0; i < MAX_tela_y; i++) {
         for (int k = 0; k < MAX_margem; k++) {
             printf(" ");
@@ -94,36 +100,103 @@ void desenharTela() {
     }
 }
 
+void desenharSelecaoJogos() {
+    limpar();
+    for (int i = 0; i < MAX_tela_y; i++) {
+        for (int k = 0; k < MAX_margem; k++) {
+            printf(" ");
+        }
+        for (int j = 0; j < MAX_tela_X; j++) {
+            if (i == 0) {
+                printf("\033[37m_\033[0m");
+            } else if (j == 0 || j == MAX_tela_X - 1) {
+                printf("|");
+            } else if (i == MAX_tela_y - 1) {
+                printf("_");
+            } else if (i == 2 && j >= 10 && j <= 26) {
+                if (j == 10) printf("SELECIONE UM JOGO");
+            } else if (i == 5 && j >= 12 && j <= 16) {
+                if (j == 12) printf("Snake");
+            } else if (i == 10 && j >= 12 && j <= 22) {
+                if (j == 12) printf("Flappy Bird");
+            } else if (i == 15 && j >= 12 && j <= 25) {
+                if (j == 12) printf("Space Invaders");
+            } else if (i == 20 && j >= 12 && j <= 17) {
+                if (j == 12) printf("Voltar");
+            } else if (j == 9 && i == opcao_selecao_jogos_y[opcao_atual]) {
+                printf("%c", forma_escolha);
+            } else {
+                printf(" ");
+            }
+        }
+        putchar('\n');
+    }
+}
 
 void mover() {
     char opcao = getch();
 
-    if (opcao == 'w' && opcao_atual > 0) {
-        opcao_atual--; 
-    } else if (opcao == 's' && opcao_atual < 2) {
-        opcao_atual++; 
-    } else if (opcao == 'q') {
-        exit(0); 
-    } else if (opcao == '\n') {
-        
-        switch (opcao_atual) {
-            case 0:
-                printf("Abrindo Ranking...\n");
-                break;
-            case 1:
-                printf("Abrindo Seleção de Jogos...\n");
-                break;
-            case 2:
-                printf("Saindo...\n");
-                exit(0);
-        }
+    switch (estado_atual) {
+        case TELA_MENU_PRINCIPAL:
+            if (opcao == 'w' && opcao_atual > 0) {
+                opcao_atual--;
+            } else if (opcao == 's' && opcao_atual < 2) {
+                opcao_atual++;
+            } else if (opcao == '\n') {
+                if (opcao_atual == 0) {
+                    printf("Abrindo Ranking...\n");
+                } else if (opcao_atual == 1) {
+                    estado_atual = TELA_SELECAO_JOGOS;
+                    opcao_atual = 0;
+                } else if (opcao_atual == 2) {
+                    estado_atual = SAIR;
+                }
+            }
+            break;
+
+        case TELA_SELECAO_JOGOS:
+            if (opcao == 'w' && opcao_atual > 0) {
+                opcao_atual--;
+            } else if (opcao == 's' && opcao_atual < 3) {
+                opcao_atual++;
+            } else if (opcao == '\n') {
+                switch (opcao_atual) {
+                    case 0:
+                        printf("Iniciando Snake...\n");
+                        break;
+                    case 1:
+                        printf("Iniciando Flappy Bird...\n");
+                        break;
+                    case 2:
+                        printf("Iniciando Space Invaders...\n");
+                        break;
+                    case 3:
+                        estado_atual = TELA_MENU_PRINCIPAL;
+                        opcao_atual = 0;
+                        break;
+                }
+            }
+            break;
+
+        case SAIR:
+            exit(0);
     }
 }
 
 int main() {
     while (1) {
-        desenharTela();
+        switch (estado_atual) {
+            case TELA_MENU_PRINCIPAL:
+                desenharMenuPrincipal();
+                break;
+            case TELA_SELECAO_JOGOS:
+                desenharSelecaoJogos();
+                break;
+            case SAIR:
+                exit(0);
+        }
         mover();
     }
+
     return 0;
 }
